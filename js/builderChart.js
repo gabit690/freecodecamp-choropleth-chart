5169259fe9de7f2b0b7f3a062be9918fb768ac5d
gabit <gabit690@gmail.com>
2021-12-01 10:36:40 -0300

    Tooltip was added and the project passes freecodecamp tests

--- js/builderChart.js
+++ js/builderChart.js
@@ -1,7 +1,7 @@
 import * as d3 from "https://cdn.skypack.dev/d3@7"
 
 const COLORS = [
-    "#ffffff", 
+    "#c0f5c3", 
     "#80ea87", 
     "#00d40e", 
     "#00ac30", 
@@ -15,6 +15,33 @@ let colorScale = d3.scaleQuantize()
                     .domain([0, 72])
                     .range(COLORS);
 
+let getCountyEducationData = (fip = 0, educationalData = []) => {
+    let countyData = {};
+    let educationalDataLength = educationalData.length;
+    for (let index = 0; index < educationalDataLength; index++) {
+        if(educationalData[index].fips === fip) {
+            countyData = educationalData[index];
+            break;
+        }
+    }
+    return countyData;
+}
+
+const addMouseOverEvent = (target = {}, mousePosition = [0, 0], data = "") => {
+    d3.select("#tooltip")
+        .attr("data-education", target.getAttribute("data-education"))
+        .style("display", "block")
+        .style("top", `${mousePosition[1] - 40}px`)
+        .style("left", `${mousePosition[0]}px`);
+    d3.select("#tooltip p")
+        .text(data);
+};
+
+const addMouseOutEvent = (target = {}) => {
+    d3.select("#tooltip")
+        .style("display", "none");
+};
+
 export default {
     chartContainer: (parentElement = "body") =>  d3.select(parentElement).append("svg"),
     getWidth: (element = {}) => {
@@ -23,25 +50,75 @@ export default {
     getHeight: (element = {}) => {
         return Number(element.style("height").slice(0,-2));
     },
+    addTitle: (parentElement = "body", content = "my title") => {
+        d3.select(parentElement)
+            .attr("id", "title")
+            .text(content);
+    },
+    addDescription: (parentElement = "body", content = "my description") => {
+        d3.select(parentElement)
+            .append("h2")
+            .attr("id", "description")
+            .text(content);
+    },
     addColorReferenceLegend: (container = {}, initialPixel = 0, margin = 0) => {
         let  ticksValues = [0, 9, 18, 27, 36, 45, 54, 63, 72];
         let heightWidth = (initialPixel - margin) / 8;
         let yScale = d3.scaleLinear([0, 72], [initialPixel, margin]);
-        let yAxis = d3.axisLeft(yScale)
+        let yAxis = d3.axisRight(yScale)
                         .tickValues(ticksValues)
                         .tickFormat(t => `${t}%`);
         container.append("g")
-                    .attr("transform", `translate(${50}, 0)`)
+                    .attr("id", "legend")
+                    .attr("transform", `translate(${1000}, 0)`)
                     .call(yAxis)
                     .selectAll("rect")
                     .data(ticksValues.filter(e => e <= 63))
                     .enter()
                     .append("rect")
-                    .attr("x", 0)
-                    .attr("y", (d, i) => yScale(d) - heightWidth)
+                    .attr("x", -20)
+                    .attr("y", d => yScale(d))
                     .attr("width", 20)
-                    .attr("height", heightWidth)
+                    .attr("height", 0)
                     .attr("stroke", "black")
-                    .attr("fill", (d, i) => colorScale(d));
+                    .attr("fill", d => colorScale(d))
+                    .transition()
+                    .delay((d, i) => i * 100)
+                    .duration(150)
+                    .attr("height", heightWidth)
+                    .attr("transform", `translate(0, ${-heightWidth})`);
+    },
+    addCounties: (container = {}, data = []) => {
+        const path = d3.geoPath();
+        const counties = topojson.feature(data[1], data[1].objects.counties);
+        let mappingData = counties.features.map(element => {
+            return {
+                feature: element,
+                education: getCountyEducationData(element.id, data[0])
+            };
+        });
+        container.selectAll('path')
+           .data(mappingData)
+           .enter()
+           .append('path')
+           .attr("class", "county")
+           .attr("data-fips", d => d.education.fips)
+           .attr("data-education", d => d.education.bachelorsOrHigher)
+           .attr('d', d => path(d.feature))
+           .attr("fill", d => colorScale(d.education.bachelorsOrHigher))
+           .on("mouseover", (event, d) => {
+                let data = `${d.education.area_name}, ${d.education.state}: ${d.education.bachelorsOrHigher}%`;
+                addMouseOverEvent(event.target, [event.clientX, event.clientY], data);
+           })
+           .on("mouseout", (event) => addMouseOutEvent(event.target));
+    },
+    addTooltip: () => {
+        d3.select("main")
+            .append("div")
+            .attr("id", "tooltip")
+            .attr("data-education", 0)
+            .append("p")
+            .style("text-transform", "capitalize")
+            .text("data-education");
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 import builderChart from "./builderChart.js";
 
-const showChoroplethChart = (educationData, countyData) => {
+const showChoroplethChart = (educationDataUrl, countyDataUrl) => {
     Promise.all([
-        fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"),
-        fetch("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
+        fetch(educationDataUrl),
+        fetch(countyDataUrl)
     ]).then(response => Promise.all(response.map(e => e.json())))
     .then(data => {
+        builderChart.addTitle("header h1", "united states educational attainment");
+        builderChart.addDescription("header", "percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
         const svg = builderChart.chartContainer("main");
         const HEIGHT = builderChart.getHeight(svg);
         const PADDING = 100;
+        builderChart.addCounties(svg, data);
         builderChart.addColorReferenceLegend(svg, HEIGHT - PADDING, PADDING);
-        // add color legend
-
+        builderChart.addTooltip();
     });
 }
 
